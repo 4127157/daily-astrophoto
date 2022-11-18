ce3c0e30087070e6b689a7eaad765cc204d8fcc2
Aryansh <89254856+4127157@users.noreply.github.com>
2022-11-18 14:18:58 +0530

    reveal on click to info

--- src/components/InfoComp.tsx
+++ src/components/InfoComp.tsx
@@ -1,3 +1,6 @@
+import { createSignal } from 'solid-js';
+import { Show } from 'solid-js';
+
 import styles from '../App.module.css';
 type AnyObj =  {
     [key: string]: any,
@@ -10,20 +13,26 @@ interface InfoProps {
 export function InfoComp(props: InfoProps) {
     console.log(props.metadata);
     //hdurl and copyright in span
+    const [clicked, setClicked] = createSignal(false);
     return (
     <div class={styles.info}>
-        <h2>{props.metadata.title}</h2>
-        <span>MORE INFO</span>
-        <span>^</span>
+        <div class={styles.infoClickable} onClick={() => setClicked(true)}>
+            <h2>{props.metadata.title}</h2>
+            <span>MORE INFO</span>
+            <span>^</span>
+        </div>
+        <Show when={clicked() === true}>
         <p>{props.metadata.explanation}</p>
         {props.metadata.hdurl ? 
             <a href={props.metadata.hdurl} target='_blank'><button>View Full Image</button></a> :
             <a href={props.metadata.url}target='_blank' ><button>View Full Image</button></a>}
-        <span>DATE: {props.metadata.date}</span>
-        <span>API VERSION: {props.metadata.service_version}</span>
-        <span>IMAGE URL: {props.metadata.url}</span>
-        {props.metadata.hdurl ? <span>HD IMAGE URL: {props.metadata.hdurl}</span> : <></>}
-        {props.metadata.copyright ? <span>COPYRIGHT: {props.metadata.copyright}</span> : <></>}
-
+        <div>
+            <span>DATE: {props.metadata.date}</span>
+            <span>API VERSION: {props.metadata.service_version}</span>
+            <span>IMAGE URL: {props.metadata.url}</span>
+            {props.metadata.hdurl ? <span>HD IMAGE URL: {props.metadata.hdurl}</span> : <></>}
+            {props.metadata.copyright ? <span>COPYRIGHT: {props.metadata.copyright}</span> : <></>}
+        </div>
+        </Show>
     </div>);
 }
