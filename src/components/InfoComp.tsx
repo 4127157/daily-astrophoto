2bf8f7cac67b654c577996681a803b993961847c
Aryansh <89254856+4127157@users.noreply.github.com>
2022-11-20 15:31:19 +0530

    add note on css

--- src/components/InfoComp.tsx
+++ src/components/InfoComp.tsx
@@ -17,21 +17,23 @@ export function InfoComp(props: InfoProps) {
     return (
     <div class={styles.info}>
         <div class={styles.infoClickable} onClick={() => setClicked(true)}>
-            <h2>{props.metadata.title}</h2>
+            <h2 class={styles.infoHeader}>{props.metadata.title}</h2>
             <span>MORE INFO</span>
             <span>^</span>
         </div>
         <Show when={clicked() === true}>
-        <p>{props.metadata.explanation}</p>
-        {props.metadata.hdurl ? 
-            <a href={props.metadata.hdurl} target='_blank'><button>View Full Image</button></a> :
-            <a href={props.metadata.url}target='_blank' ><button>View Full Image</button></a>}
-        <div>
-            <span>DATE: {props.metadata.date}</span>
-            <span>API VERSION: {props.metadata.service_version}</span>
-            <span>IMAGE URL: {props.metadata.url}</span>
-            {props.metadata.hdurl ? <span>HD IMAGE URL: {props.metadata.hdurl}</span> : <></>}
-            {props.metadata.copyright ? <span>COPYRIGHT: {props.metadata.copyright}</span> : <></>}
+        <div class={styles.infoExpand}>
+            <p>{props.metadata.explanation}</p>
+            {props.metadata.hdurl ? 
+                <a href={props.metadata.hdurl} target='_blank'><button>View Full Image</button></a> :
+                <a href={props.metadata.url}target='_blank' ><button>View Full Image</button></a>}
+            <div>
+                <span>DATE: {props.metadata.date}</span>
+                <span>API VERSION: {props.metadata.service_version}</span>
+                <span>IMAGE URL: {props.metadata.url}</span>
+                {props.metadata.hdurl ? <span>HD IMAGE URL: {props.metadata.hdurl}</span> : <></>}
+                {props.metadata.copyright ? <span>COPYRIGHT: {props.metadata.copyright}</span> : <></>}
+            </div>
         </div>
         </Show>
     </div>);
