6ccebdf7fbffb0019bd54b661048f0a5eb07fe5c
Aryansh <89254856+4127157@users.noreply.github.com>
2022-11-29 20:19:19 +0530

    made final changes

--- src/components/InfoComp.tsx
+++ src/components/InfoComp.tsx
@@ -1,10 +1,12 @@
 import { createEffect, createSignal, Match, onMount, Switch } from 'solid-js';
 import { Show } from 'solid-js';
+import { useWindowSize } from '@solid-primitives/resize-observer';
 import ExpandMoreIcon from '@suid/icons-material/ExpandMore';
 import ExpandLessIcon from '@suid/icons-material/ExpandLess';
 import anime from 'animejs/lib/anime.es.js';
 
 import styles from '../App.module.css';
+import { Yard } from '@suid/icons-material';
 type AnyObj =  {
     [key: string]: any,
 }
@@ -13,24 +15,29 @@ interface InfoProps {
     metadata: AnyObj,
 }
 
+const size = useWindowSize();
+
 export function InfoComp(props: InfoProps) {
-    console.log(props.metadata);
-    //hdurl and copyright in span
-    // const [clicked, setClicked] = createSignal(false);
     const [toggle, setToggle] = createSignal(false);
-    let infoDiv!: HTMLDivElement; 
     const [animComplete, setAnimComplete] = createSignal(false, {equals: false});
+    let infoDiv!: HTMLDivElement; 
     let infoExpandDiv!: HTMLDivElement;
     let infoExpandChildDiv!: HTMLDivElement;
+    let toAnimate!: boolean;
+    let toAnimateSecondary!: boolean;
+    let expandElemHeight!: number;
 
-    function processAnimations(close: boolean){
-        playAnim(close);
+    let processMainAnim = (close: boolean) => {
+        if(toAnimate){
+            playAnim(close);
+        }
+        toAnimate = true; //Quick hacky solution to not trigger animation for the first render
         function playAnim(close: boolean) {
             let animation = anime({
                 targets: infoDiv,
                 translateY: () => {
-                    if(!close) {
-                        return '-40vh';
+                    if(close) {
+                        return '-250px';
                     } else {
                         return '0';
                     }
@@ -39,24 +46,79 @@ export function InfoComp(props: InfoProps) {
                 easing: 'easeInOutCirc',
                 });
 
-            animation.play;
             animation.finished.then(() => setAnimComplete(!animComplete()));
         }
-        if(infoExpandDiv){
-            console.log(infoExpandDiv.offsetHeight);
+    }
+
+    let processSecondaryAnim = (bool: boolean) => {
+
+        if(toAnimateSecondary && bool){
+            expandElemHeight = infoExpandChildDiv.offsetHeight;
+            if(infoExpandDiv.style.opacity != '1'){
+                infoExpandDiv.style.height = '0';
+                infoExpandDiv.style.opacity = '1';
+            }
+
+            let animation = anime.timeline({
+                duration: 300,
+            });
+
+            animation.add({
+                targets: infoExpandDiv,
+                height: () => {
+                    if(toggle()) {
+                        return expandElemHeight;
+                    } /*else {
+                        return 0;
+                    }*/
+                },
+                opacity: () => {
+                    if(!toggle()){
+                        return 0;
+                    }
+                },
+                easing: 'easeInOutCirc',
+            });
+
+            animation.add({
+                targets: infoExpandChildDiv,
+                opacity: () => {
+                    if(toggle()) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                },
+                easing: 'easeInSine',
+            }, '-=200');
+
+            animation.play;
+        }
+
+        toAnimateSecondary = true;
+    }
+
+    let setForceElemHeight = (width: number, height: number) => {
+        if(infoExpandDiv && infoExpandChildDiv){
+            anime({
+                targets: infoExpandDiv,
+                height: infoExpandChildDiv.offsetHeight,
+                duration: 10,
+            });
         }
     }
     
-    createEffect(() => processAnimations(toggle()))
+    createEffect( () => processMainAnim(toggle()));
+    createEffect( () => processSecondaryAnim(animComplete()));
+    createEffect( () => setForceElemHeight(size.width, size.height));
 
     return (
     <div class={styles.info} ref={infoDiv} >
         <div class={styles.infoClickable} onClick={() =>{
-                //playAnim(toggle());
                 setToggle(!toggle());
             }}>
             <h2 class={styles.infoHeader}>{props.metadata.title}</h2>
-            <span>MORE INFO</span>
+            <span class={styles.moreInfoSpan}>MORE INFO</span>
             <Switch>
                 <Match when={animComplete() === true}>
                     <ExpandMoreIcon sx={{ fontSize: 40 }} style={ 'display: block; margin:auto;'}  class={styles.infoMaterialExpand}/>
@@ -65,20 +127,19 @@ export function InfoComp(props: InfoProps) {
                     <ExpandLessIcon sx={{ fontSize: 40 }} style={ 'display: block; margin:auto;'} class={styles.infoMaterialExpand}/>
                 </Match>
             </Switch>
-            
         </div>
         <div class={styles.infoExpand} ref={infoExpandDiv}>
             <div class={styles.infoExpandChild} ref={infoExpandChildDiv}>
                 <p class={styles.infoExplanation}>{props.metadata.explanation}</p>
                 {props.metadata.hdurl ? 
-                    <a href={props.metadata.hdurl} target='_blank'><button>View Full Image</button></a> :
-                    <a href={props.metadata.url}target='_blank' ><button>View Full Image</button></a>}
-                <div>
+                    <a href={props.metadata.hdurl} target='_blank'><button class={styles.fullImgBtn}>View Full Image</button></a> :
+                    <a href={props.metadata.url}target='_blank' ><button class={styles.fullImgBtn}>View Full Image</button></a>}
+                <div class={styles.metaInfoDiv}>
                     <span>DATE: {props.metadata.date}</span>
                     <span>API VERSION: {props.metadata.service_version}</span>
                     {props.metadata.copyright ? <span>COPYRIGHT: {props.metadata.copyright}</span> : <></>}
-                    <span>IMAGE URL: {props.metadata.url}</span>
-                    {props.metadata.hdurl ? <span>HD IMAGE URL: {props.metadata.hdurl}</span> : <></>}
+                    <span>IMAGE URL: <a href={props.metadata.url} target='_blank'>link</a></span>
+                    {props.metadata.hdurl ? <span>HD IMAGE URL: <a href={props.metadata.hdurl} target='_blank'>link</a></span> : <></>}
                 </div>
             </div>
         </div>
